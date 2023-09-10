1e30571c7e2cce796c166cb7fe9d332332cb3f78
asmsu12920220151 <144580459+asmsu12920220151@users.noreply.github.com>
2023-09-10 11:51:38 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,12 @@
+def checkleap(year):
+  
+  if((year % 400 == 0) or
+     (year % 100 != 0) and
+     (year % 4 == 0)):
+    print("given year is a leap year");
+  else:
+    print("given year is not a leap year:");
+
+year=int(input("Enter the number: "))
+checkleap(year);
+  
